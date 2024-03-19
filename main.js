8096a76ef715c9d126f212983202bea2eae0c8ae
anyuz <zhengannyu@gmail.com>
2024-03-20 02:07:12 +0800

    新增收縮率變數

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let btn = document.getElementById("submit");
+// 燒製後容量/高變數
 let firedMl = document.getElementById("firedMl");
 let firedMl2 = document.getElementById("firedMl2");
 let firedWidth = document.getElementById("firedWidth");
@@ -6,9 +7,11 @@ let firedHeigh = document.getElementById("firedHeigh");
 btn.addEventListener("click", function () {
   let r = parseInt(document.getElementById("width").value);
   let h = parseInt(document.getElementById("height").value);
-  let FiredMl = ((r * 0.5 * 0.88) ** 2 * 3.14 * h * 0.88) / 1000;
-  let FiredWidth = r * 0.88;
-  let FiredHeigh = h * 0.88;
+  let p = parseInt(document.getElementById("pa").value);
+  let pa = (100 - p) / 100;
+  let FiredMl = ((r * 0.5 * pa) ** 2 * 3.14 * h * pa) / 1000;
+  let FiredWidth = r * pa;
+  let FiredHeigh = h * pa;
   firedMl.innerHTML = FiredMl.toFixed(2);
   firedWidth.innerHTML = FiredWidth.toFixed(2);
   firedHeigh.innerHTML = FiredHeigh.toFixed(2);
